7e86e4fa982f79667db2814f071f27f8ebdead0e
bigonil <luca.bigoni@gmail.com>
2024-10-13 12:33:50 +0200

    Add outputs.tf file to the repository with the following content: vpc_id = "vpc-0a1b2c3d4e5f67890" subnet_id = "subnet-0a1b2c3d4e5f67890" security_group_id = "sg-0a1b2c3d4e5f67890" app_instance_id = "i-0a1b2c3d4e5f67890" db_instance_id = "i-0a1b2c3d4e5f67891"

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,27 @@
 # SPDX-License-Identifier: MPL-2.0
 
 # Output declarations
+output "vpc_id" {
+  description = "ID of project VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnet_ids" {
+  description = "IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "lb_url" {
+  description = "URL of load balancer"
+  value       = "http://${module.elb_http.elb_dns_name}/"
+}
+
+output "web_server_count" {
+  description = "Number of web servers provisioned"
+  value       = length(module.ec2_instances.instance_ids)
+}
